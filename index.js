e1e30212419fd7f9d5b0a88628a0a5e4b5c952b1
lycho33 <cho.lyds@gmail.com>
2021-08-31 15:27:36 -0400

    commit tree 283f73abe3950437eb94f4fcca34bc71616e555d

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+// const num1 = 31
+// const num2 = 2
+// const num3 = 19
+// const num4 = 5
+// let multiply = num1 * num2
+// let random = Math.floor(Math.random() * 10) + 1;
+// let mod = num3 % num4
+// let max = Math.max(1,2,3,5,8,20)
+
+let num1 = 31
+let num2 = 2
+let num3 = 19
+let num4 = 5
+
+let multiply = num1 * num2
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = num3 % num4
+let max = Math.max(1,2,5,8,20)
